dd3b4b29e212245c583b8d477a0b2237f05645e6
rifqinanda27 <rifqinanda60@gmail.com>
2025-05-19 09:00:34 +0700

    add_table:instruksi, detail_resep

---
+++ database/migrations/2025_05_19_013507_create_detail_resep_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_resep', function (Blueprint $table) {
+            $table->id('id_detail_resep');
+
+            $table->unsignedBigInteger('id_resep');
+            $table->unsignedBigInteger('id_obat');
+            $table->unsignedBigInteger('id_instruksi');
+
+            $table->timestamps();
+
+            // Foreign keys
+            $table->foreign('id_resep')->references('id_resep')->on('resep')->onDelete('cascade');
+            $table->foreign('id_obat')->references('id_obat')->on('obat')->onDelete('restrict');
+            $table->foreign('id_instruksi')->references('id_instruksi')->on('instruksi')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_resep');
+    }
+};
